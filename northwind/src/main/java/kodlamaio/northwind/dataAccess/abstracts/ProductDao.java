ffbebeec0a36c182478ad1c2a6e7e1dc2c0e1226
umutyildiz <56760679+umutyildiz1@users.noreply.github.com>
2021-05-31 01:00:58 +0300

    week8

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,26 @@
 package kodlamaio.northwind.dataAccess.abstracts;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import kodlamaio.northwind.entities.concretes.Category;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product,Integer>{
-
+	Product getByProductName(String productName);
+	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
+	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
+	
+	//select * from product where category_id in (1,2,3,4)
+	List<Product> getByCategory_CategoryIdIn(List<Integer> categories);
+	
+	List<Product> getByProductNameContains(String productName);
+	
+	List<Product> getByProductNameStartsWith(String productName);
+	
+	
+	@Query("From Product where productName=:productName and category.categoryId=:categoryId")//=:öncesi Entity class attributü sonrası parametre ismi
+	List<Product> getByNameAndCategory(String productName, int categoryId);
 }
